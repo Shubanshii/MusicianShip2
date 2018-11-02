32ac24f6687da65efdf9c5eaadf64ccbe0646a6b
Shubanshii <c_day@alumni.baylor.edu>
2018-11-01 22:13:42 -0500

    trying tests again

--- test/test-server.js
+++ test/test-server.js
@@ -11,99 +11,99 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-// describe('Signing up, protected routes, etc.', function() {
-//   const userCredentials = {
-//     email: 'sponge@bob.com',
-//     password: 'garyTheSnail'
-//   };
-//
-//   const badEmailCredentials = {
-//     email: 'spongebob.com',
-//     password: 'garyTheSnail'
-//   };
-//
-//   const badPasswordCredentials = {
-//     email: 'sponge1@bob1.com',
-//     password: 'g'
-//   };
-//
-//   before(function() {
-//     return runServer(TEST_DATABASE_URL);
-//   });
-//
-//   after(function() {
-//     return closeServer();
-//   });
-//
-//   it('should not get profile if not logged in', function() {
-//     return chai
-//       .request(app)
-//       .get('/profile')
-//       .then((res) => {
-//         expect(res.text).to.include('Login or Register');
-//         expect(res.redirects).to.not.include('/profile');
-//       })
-//   });
-//
-//   it('should reject users with invalid email', function() {
-//     return chai
-//       .request(app)
-//       .post("/signup")
-//       .send(badEmailCredentials)
-//       .then(() => {
-//         User.findOne({ 'local.email': badEmailCredentials.email }, function(err, user) {
-//           expect(user).to.equal(null);
-//         });
-//       });
-//   });
-//
-//   it('should reject users with invalid password', function() {
-//     return chai
-//       .request(app)
-//       .post("/signup")
-//       .send(badPasswordCredentials)
-//       .then(() => {
-//         User.findOne({ 'local.email': badPasswordCredentials.email }, function(err, user) {
-//           expect(user).to.equal(null);
-//         });
-//       });
-//   });
-//
-//   describe('sign up and get profile', function() {
-//     var agent = chai.request.agent(app);
-//     it('should sign up users with valid email and password', function() {
-//       return agent
-//         .post('/signup')
-//         .send(userCredentials)
-//         .then((res) => {
-//           User.findOne({ 'local.email': userCredentials.email }, function(err, user) {
-//             if (user) {
-//               expect(user.local.email).to.equal(userCredentials.email);
-//             }
-//           });
-//           expect(res.redirects[0]).to.include('/profile');
-//           chai.request(app).get('/logout');
-//           return User.remove({'local.email': userCredentials.email});
-//         })
-//     });
-//     it('should get profile if user signed up correctly', function() {
-//       return agent
-//         .post('/signup')
-//         .send(userCredentials)
-//         .then(() => {
-//           return agent
-//             .get('/profile')
-//             .then((res) => {
-//               return User.remove({'local.email': userCredentials.email});
-//             });
-//         })
-//     });
-//   });
-//
-//
-//
-//
-// });
+describe('Signing up, protected routes, etc.', function() {
+  const userCredentials = {
+    email: 'sponge@bob.com',
+    password: 'garyTheSnail'
+  };
+
+  const badEmailCredentials = {
+    email: 'spongebob.com',
+    password: 'garyTheSnail'
+  };
+
+  const badPasswordCredentials = {
+    email: 'sponge1@bob1.com',
+    password: 'g'
+  };
+
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should not get profile if not logged in', function() {
+    return chai
+      .request(app)
+      .get('/profile')
+      .then((res) => {
+        expect(res.text).to.include('Login or Register');
+        expect(res.redirects).to.not.include('/profile');
+      })
+  });
+
+  it('should reject users with invalid email', function() {
+    return chai
+      .request(app)
+      .post("/signup")
+      .send(badEmailCredentials)
+      .then(() => {
+        User.findOne({ 'local.email': badEmailCredentials.email }, function(err, user) {
+          expect(user).to.equal(null);
+        });
+      });
+  });
+
+  it('should reject users with invalid password', function() {
+    return chai
+      .request(app)
+      .post("/signup")
+      .send(badPasswordCredentials)
+      .then(() => {
+        User.findOne({ 'local.email': badPasswordCredentials.email }, function(err, user) {
+          expect(user).to.equal(null);
+        });
+      });
+  });
+
+  describe('sign up and get profile', function() {
+    var agent = chai.request.agent(app);
+    it('should sign up users with valid email and password', function() {
+      return agent
+        .post('/signup')
+        .send(userCredentials)
+        .then((res) => {
+          User.findOne({ 'local.email': userCredentials.email }, function(err, user) {
+            if (user) {
+              expect(user.local.email).to.equal(userCredentials.email);
+            }
+          });
+          expect(res.redirects[0]).to.include('/profile');
+          chai.request(app).get('/logout');
+          return User.remove({'local.email': userCredentials.email});
+        })
+    });
+    it('should get profile if user signed up correctly', function() {
+      return agent
+        .post('/signup')
+        .send(userCredentials)
+        .then(() => {
+          return agent
+            .get('/profile')
+            .then((res) => {
+              return User.remove({'local.email': userCredentials.email});
+            });
+        })
+    });
+  });
+
+
+
+
+});
 
 describe("index page", function() {
   it("should exist", function() {
